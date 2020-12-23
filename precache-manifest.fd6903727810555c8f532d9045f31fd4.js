cbf61fcc7db4f82ab97714b8dd7a13e297291795
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2020-12-23 20:44:00 +0600

    Updates

---
+++ precache-manifest.fd6903727810555c8f532d9045f31fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a402778638d4305c4ecd0d2671507f8",
+    "url": "/monstrats-rolodex/index.html"
+  },
+  {
+    "revision": "1afbf969227d205a2c15",
+    "url": "/monstrats-rolodex/static/css/main.837e3f0b.chunk.css"
+  },
+  {
+    "revision": "d1b6a25d023fe62e371c",
+    "url": "/monstrats-rolodex/static/js/2.7c1f2a78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstrats-rolodex/static/js/2.7c1f2a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afbf969227d205a2c15",
+    "url": "/monstrats-rolodex/static/js/main.a1ad06b1.chunk.js"
+  },
+  {
+    "revision": "959892d5d7d851b116a5",
+    "url": "/monstrats-rolodex/static/js/runtime-main.ed1dde39.js"
+  }
+]);
